17d376fde6b3e8a4d01d2716ced6630cc53ff720
QBos07 <62326551+QBos07@users.noreply.github.com>
2021-11-24 08:37:15 +0100

    Add console option

--- src/main/kotlin/qbos/ev3fx/test1/cmdinterface/main.kt
+++ src/main/kotlin/qbos/ev3fx/test1/cmdinterface/main.kt
@@ -1,5 +1,8 @@
 package qbos.ev3fx.test1.cmdinterface
 
-fun main(vararg args: String) {
+import com.example.MyApp
+import tornadofx.launch
 
+fun main(args: Array<String>) {
+    if (args.isEmpty() || (args[1] != "--console") || (args[1] != "-c")) launch<MyApp>(args)
 }
